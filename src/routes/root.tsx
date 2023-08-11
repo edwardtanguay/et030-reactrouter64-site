b012b4fa63e028ceceb68e4ef9d14d0fa5e4c53b
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-11 09:12:59 +0200

    fixed ts

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,5 +1,5 @@
 import { Link, Outlet } from "react-router-dom";
-import { getContacts } from '../contacts'; 
+// import { getContacts } from '../contacts'; 
 
 export default function Root() {
 	return (
