b4ad7174cc2f316d8e954a92ceeab7f1e0965a36
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-13 23:03:33 +0200

    it works

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import {
 } from "react-router-dom";
 import Root, { loader as rootLoader, action as rootAction } from './routes/root';
 import ErrorPage from './error-page';
-import Contact, {loader as contactLoader, action as contactAction} from './routes/contact';
-import EditContact, {action as editAction} from './routes/edit';
+import Contact, { loader as contactLoader, action as contactAction } from './routes/contact';
+import EditContact, { action as editAction } from './routes/edit';
 import { action as deleteAction } from './routes/delete';
 import Index from './routes';
 
@@ -20,25 +20,30 @@ const router = createBrowserRouter([
 		action: rootAction,
 		children: [
 			{
-				index: true,
-				element: < Index />
-			},
-			{
-				path: "contacts/:contactId",
-				element: <Contact />,
-				loader: contactLoader,
-				action: contactAction
-			},
-			{
-				path: "contacts/:contactId/edit",
-				element: <EditContact />,
-				loader: contactLoader,
-				action: editAction
-			},
-			{
-				path: 'contacts/:contactId/delete',
-				action: deleteAction,
-				errorElement: <div>There was an error while deleting.</div>
+				errorElement: <ErrorPage/>,
+				children: [
+					{
+						index: true,
+						element: < Index />
+					},
+					{
+						path: "contacts/:contactId",
+						element: <Contact />,
+						loader: contactLoader,
+						action: contactAction
+					},
+					{
+						path: "contacts/:contactId/edit",
+						element: <EditContact />,
+						loader: contactLoader,
+						action: editAction
+					},
+					{
+						path: 'contacts/:contactId/delete',
+						action: deleteAction,
+						errorElement: <div>There was an error while deleting.</div>
+					}
+				]
 			}
 		]
 	}
